6a317c81eb14b9d1acfa1bbad52706477ecd9955
rloss <chnnsx@gmail.com>
2025-05-10 00:50:01 +0900

    app tsx

--- mydevatlas/src/App.tsx
+++ mydevatlas/src/App.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Map from "./pages/Map";
+import SubMap from "./pages/SubMap";
 
 export default function App() {
-  return <div className="p-4">MyDevAtlas</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/map/:category" element={<SubMap />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }

6690fde95568ccb4b36f3d00c0e4a79ae6d322c3
rloss <chnnsx@gmail.com>
2025-05-10 01:05:23 +0900

    vite config ts

--- mydevatlas/vite.config.ts
+++ mydevatlas/vite.config.ts
@@ -1,6 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ 상대 경로로 설정 (정적 호스팅 대응)
 });
+
